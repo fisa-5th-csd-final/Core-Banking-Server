427925d0e8776271f8228036c0f906521260772a
Bellmin <whdals011203@naver.com>
2025-10-24 16:28:48 +0900

    refactor: 사용하지 않는 메소드 삭제

--- bank/src/main/java/com/fisa/bank/common/presentation/response/ApiResponseGenerator.java
+++ bank/src/main/java/com/fisa/bank/common/presentation/response/ApiResponseGenerator.java
@@ -7,21 +7,6 @@ import com.fisa.bank.common.presentation.response.code.ResponseCode;
 public class ApiResponseGenerator {
 
     /**
-     * 200 OK 응답을 만들고 싶을 때 사용하는 static 메소드
-     * @param body
-     * @return
-     * @param <T>
-     */
-    public static <T> ApiResponse<SuccessBody<T>> ok(T body){
-        ApiResponseCode responseCode = ResponseCode.GET;
-
-        return new ApiResponse<>(
-                responseCode.getStatus(),
-                new SuccessBody<>(responseCode.getCode(), responseCode.getMessage(), body)
-        );
-    }
-
-    /**
      *
      * @param responseCode
      * @param body
