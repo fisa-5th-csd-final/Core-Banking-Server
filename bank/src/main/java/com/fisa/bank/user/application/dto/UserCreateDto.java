0242a12aeec1a176927a4b412e091e0f964e78de
Bellmin <whdals011203@naver.com>
2025-10-24 17:01:42 +0900

    feat: UserCreateDto 정의

---
+++ bank/src/main/java/com/fisa/bank/user/application/dto/UserCreateDto.java
@@ -0,0 +1,19 @@
+package com.fisa.bank.user.application.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fisa.bank.common.presentation.util.EpochTimeToLocalDateTimeSerializer;
+import java.time.LocalDateTime;
+
+public record UserCreateDto(
+
+        @JsonDeserialize(using = EpochTimeToLocalDateTimeSerializer.class)
+        LocalDateTime birthday,
+
+        String name,
+        String loginId,
+        String password,
+        String address,
+        String job,
+        long salary
+) {
+}
