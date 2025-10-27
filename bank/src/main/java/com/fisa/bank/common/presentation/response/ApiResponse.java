56259db43422d232b74be49f9622f2312d3bfb6b
Bellmin <whdals011203@naver.com>
2025-10-27 16:21:30 +0900

    refactor: 불필요한 import 삭제

--- bank/src/main/java/com/fisa/bank/common/presentation/response/ApiResponse.java
+++ bank/src/main/java/com/fisa/bank/common/presentation/response/ApiResponse.java
@@ -1,7 +1,6 @@
 package com.fisa.bank.common.presentation.response;
 
 import com.fisa.bank.common.presentation.response.body.ApiResponseBody;
-import javax.swing.Spring;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
