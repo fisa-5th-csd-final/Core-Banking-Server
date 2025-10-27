792990c0049c173c7ddb4ce80556575c5ca8c592
Bellmin <whdals011203@naver.com>
2025-10-27 09:49:33 +0900

    feat: 회원가입 로직 추가

---
+++ bank/src/main/java/com/fisa/bank/user/application/service/UserService.java
@@ -0,0 +1,36 @@
+package com.fisa.bank.user.application.service;
+
+import com.fisa.bank.user.application.dto.UserCreateDto;
+import com.fisa.bank.user.application.util.PasswordUtil;
+import com.fisa.bank.user.persistence.entity.User;
+import com.fisa.bank.user.persistence.repository.UserRepository;
+import java.math.BigInteger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordUtil passwordUtil;
+
+    @Transactional
+    public boolean create(UserCreateDto request){
+        String encryptedPassword = passwordUtil.encrypt(request.password());
+
+        User user = User.create(
+                request.name(),
+                request.address(),
+                request.birthday(),
+                BigInteger.valueOf(request.salary()),
+                request.loginId(),
+                encryptedPassword);
+
+        userRepository.save(user);
+
+        return true;
+    }
+
+}
