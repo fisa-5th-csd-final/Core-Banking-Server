8b71890111325c8432f39ae7fa421f288e7969a5
Bellmin <whdals011203@naver.com>
2025-10-27 12:08:18 +0900

    refactor: validation 추가

--- bank/src/main/java/com/fisa/bank/user/application/dto/UserCreateRequest.java
+++ bank/src/main/java/com/fisa/bank/user/application/dto/UserCreateRequest.java
@@ -2,18 +2,21 @@ package com.fisa.bank.user.application.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fisa.bank.common.presentation.util.EpochTimeToLocalDateTimeSerializer;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.time.LocalDateTime;
 
 public record UserCreateRequest(
 
         @JsonDeserialize(using = EpochTimeToLocalDateTimeSerializer.class)
-        LocalDateTime birthday,
+        @NotBlank @NotNull @PositiveOrZero LocalDateTime birthday,
 
-        String name,
-        String loginId,
-        String password,
-        String address,
-        String job,
-        long salary
+        @NotBlank @NotNull String name,
+        @NotBlank @NotNull String loginId,
+        @NotBlank @NotNull String password,
+        @NotBlank @NotNull String address,
+        @NotBlank @NotNull String job,
+        @PositiveOrZero long salary
 ) {
 }
