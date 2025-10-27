64cf657c8fa248bf240ee50d6b95dfb873f80634
Bellmin <whdals011203@naver.com>
2025-10-27 14:24:03 +0900

    refactor: BusinessException 적용

--- bank/src/main/java/com/fisa/bank/user/application/exception/InvalidPasswordFormatException.java
+++ bank/src/main/java/com/fisa/bank/user/application/exception/InvalidPasswordFormatException.java
@@ -1,10 +1,13 @@
 package com.fisa.bank.user.application.exception;
 
-public class InvalidPasswordFormatException extends RuntimeException {
+import com.fisa.bank.common.application.exception.BusinessException;
 
+public class InvalidPasswordFormatException extends BusinessException {
+
+    private static final String errorCode = "1001";
     private static final String message = "비밀번호 규칙에 맞지 않습니다.";
 
     public InvalidPasswordFormatException() {
-        super(message);
+        super(errorCode, message);
     }
 }
