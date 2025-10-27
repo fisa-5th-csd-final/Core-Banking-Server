6431110d11c94334c2a90e7ebd01d516d5d380e7
Bellmin <whdals011203@naver.com>
2025-10-27 15:07:36 +0900

    refactor: Exception 객체 싱글톤 추가
    - 생성자 private 으로 변경

--- bank/src/main/java/com/fisa/bank/user/application/exception/InvalidAuthInfoException.java
+++ bank/src/main/java/com/fisa/bank/user/application/exception/InvalidAuthInfoException.java
@@ -4,10 +4,12 @@ import com.fisa.bank.common.application.exception.BusinessException;
 
 public class InvalidAuthInfoException extends BusinessException {
 
+    public static final InvalidAuthInfoException EXCEPTION = new InvalidAuthInfoException();
+
     private static final String errorCode = "1000";
     private static final String message = "이미 존재하는 ID 입니다.";
 
-    public InvalidAuthInfoException() {
+    private InvalidAuthInfoException() {
         super(errorCode, message);
     }
 
