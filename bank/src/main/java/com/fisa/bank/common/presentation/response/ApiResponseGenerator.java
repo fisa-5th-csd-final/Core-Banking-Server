51554e1c2b313b9c90a0f0d3918e55fb3b0974b8
Bellmin <whdals011203@naver.com>
2025-10-27 11:57:24 +0900

    feat: ApiResponseGenerator 메소드 추가
    - (status, messageCode, body)
    - (responseCode)

--- bank/src/main/java/com/fisa/bank/common/presentation/response/ApiResponseGenerator.java
+++ bank/src/main/java/com/fisa/bank/common/presentation/response/ApiResponseGenerator.java
@@ -2,7 +2,9 @@ package com.fisa.bank.common.presentation.response;
 
 import com.fisa.bank.common.presentation.response.body.SuccessBody;
 import com.fisa.bank.common.presentation.response.code.ApiResponseCode;
+import com.fisa.bank.common.presentation.response.code.MessageCode;
 import com.fisa.bank.common.presentation.response.code.ResponseCode;
+import org.springframework.http.HttpStatus;
 
 public class ApiResponseGenerator {
 
@@ -13,7 +15,7 @@ public class ApiResponseGenerator {
      * @return
      * @param <T>
      */
-    public static <T> ApiResponse<SuccessBody<T>> create(ApiResponseCode responseCode, T body){
+    public static <T> ApiResponse<SuccessBody<T>> success(ApiResponseCode responseCode, T body){
 
         String code = responseCode.getCode();
         String message = responseCode.getMessage();
@@ -22,6 +24,22 @@ public class ApiResponseGenerator {
                 responseCode.getStatus(),
                 new SuccessBody<>(code, message, body)
         );
+    }
+
+    public static <T> ApiResponse<SuccessBody<T>> success(ApiResponseCode responseCode){
+        String code = responseCode.getCode();
+        String message = responseCode.getMessage();
+
+        return new ApiResponse<>(
+                responseCode.getStatus(),
+                new SuccessBody<>(code, message, null)
+        );
+    }
+
+    public static <T> ApiResponse<SuccessBody<T>> success (HttpStatus status, MessageCode messageCode, T body){
+        String code = messageCode.getCode();
+        String message = messageCode.getMessage();
+        return new ApiResponse<>(status, new SuccessBody<>(code, message, body));
     }
 
 }
