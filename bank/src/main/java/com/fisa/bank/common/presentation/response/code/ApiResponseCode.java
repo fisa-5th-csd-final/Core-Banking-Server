640bb049cc9040b22064cab9f7b6167783bf1807
Bellmin <whdals011203@naver.com>
2025-10-27 14:59:45 +0900

    comment: TODO 주석 추가

--- bank/src/main/java/com/fisa/bank/common/presentation/response/code/ApiResponseCode.java
+++ bank/src/main/java/com/fisa/bank/common/presentation/response/code/ApiResponseCode.java
@@ -2,6 +2,7 @@ package com.fisa.bank.common.presentation.response.code;
 
 import org.springframework.http.HttpStatus;
 
+// TODO: HttpStatus 의존성 제거하기
 public interface ApiResponseCode {
 
     public HttpStatus getStatus();
