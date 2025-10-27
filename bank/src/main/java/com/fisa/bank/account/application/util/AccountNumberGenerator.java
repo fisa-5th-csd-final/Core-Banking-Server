bfe6cb190f457a8ee87ba31861a79a9af7f6d333
Ju-Min-Kyung <alsrud7767@naver.com>
2025-10-27 14:45:26 +0900

    AccountNumberGenerator

--- bank/src/main/java/com/fisa/bank/account/application/util/AccountNumberGenerator.java
+++ bank/src/main/java/com/fisa/bank/account/application/util/AccountNumberGenerator.java
@@ -1,4 +1,22 @@
-package com.fisa.bank.account.application.util;
+package com.fisa.bank.account.application.util; /**
+ * 계좌번호 생성 유틸
+ *
+ * 형식: XXXXYYYZZZZZZ (총 13자리 숫자)
+ * 예시: 4821307591204
+ */
+import java.security.SecureRandom;
 
 public class AccountNumberGenerator {
+
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generate() {
+        StringBuilder sb = new StringBuilder(13);
+
+        for (int i = 0; i < 13; i++) {
+            sb.append(random.nextInt(10)); // 0~9
+        }
+
+        return sb.toString(); // 하이픈 없이 반환
+    }
 }
