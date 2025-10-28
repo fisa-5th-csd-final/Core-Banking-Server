52d2c575bcffbb3fac1e234045cca9a29f50c77e
cheongjay <wjdjoonim@gmail.com>
2025-10-28 15:49:50 +0900

    refactor: DTO파일명에서 DTO 빼기

--- bank/src/main/java/com/fisa/bank/loan/application/service/LoanService.java
+++ bank/src/main/java/com/fisa/bank/loan/application/service/LoanService.java
@@ -1,9 +1,8 @@
 package com.fisa.bank.loan.application.service;
 
-import com.fisa.bank.loan.application.dto.request.LoanProductCreateRequestDTO;
-import com.fisa.bank.loan.application.dto.response.LoanProductCreateResponseDTO;
+import com.fisa.bank.loan.application.dto.request.LoanProductCreateRequest;
+import com.fisa.bank.loan.application.dto.response.LoanProductCreateResponse;
 import com.fisa.bank.loan.persistence.entity.LoanProduct;
-import com.fisa.bank.loan.persistence.enums.LoanType;
 import com.fisa.bank.loan.persistence.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,14 +13,14 @@ public class LoanService {
     private final LoanRepository loanRepository;
 
     @org.springframework.transaction.annotation.Transactional
-    public LoanProductCreateResponseDTO createLoanProduct(LoanProductCreateRequestDTO requestDTO){
+    public LoanProductCreateResponse createLoanProduct(LoanProductCreateRequest requestDTO){
 
         LoanProduct loanProduct = loanRepository.save(LoanProduct.builder()
                 .name(requestDTO.getName())
                 .type(requestDTO.getType())
                 .build());
 
-        LoanProductCreateResponseDTO response = LoanProductCreateResponseDTO.builder()
+        LoanProductCreateResponse response = LoanProductCreateResponse.builder()
                 .name(loanProduct.getName())
                 .type(loanProduct.getType())
                 .loanProductId(loanProduct.getLoanProductId())
