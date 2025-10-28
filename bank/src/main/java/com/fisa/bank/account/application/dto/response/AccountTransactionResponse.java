3c31631ffebc47f57d068c17ff3c669d6df806df
Ju-Min-Kyung <alsrud7767@naver.com>
2025-10-28 12:21:15 +0900

    add: 입금과 출금에 필요한 request, response 작성

---
+++ bank/src/main/java/com/fisa/bank/account/application/dto/response/AccountTransactionResponse.java
@@ -0,0 +1,32 @@
+package com.fisa.bank.account.application.dto.response;
+
+import com.fisa.bank.account.persistence.entity.TransactionAccountEntity;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Builder
+public record AccountTransactionResponse(
+        String transactionId,     // 거래 ID
+        Long accountId,           // 계좌 ID
+        String type,              // 거래 타입 (DEPOSIT, WITHDRAW)
+        BigDecimal amount,        // 거래 금액
+        BigDecimal balanceBefore, // 거래 전 잔액
+        BigDecimal balanceAfter,  // 거래 후 잔액
+        boolean isIncome,         // 입금 여부
+        LocalDateTime transactionAt // 거래 일시
+) {
+    public static AccountTransactionResponse from(TransactionAccountEntity entity) {
+        return AccountTransactionResponse.builder()
+                .transactionId(entity.getTrxAId())
+                .accountId(entity.getAccount().getAccountId().getValue())
+                .type(entity.getType().name())
+                .amount(entity.getAmount())
+                .balanceBefore(entity.getBalanceBefore())
+                .balanceAfter(entity.getBalanceAfter())
+                .isIncome(entity.getIsIncome())
+                .transactionAt(entity.getCreatedAt())
+                .build();
+    }
+}
