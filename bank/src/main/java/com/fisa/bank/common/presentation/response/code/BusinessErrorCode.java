471a882f4f4a84973d7e1edc463214d137796393
Bellmin <whdals011203@naver.com>
2025-10-28 15:48:58 +0900

    refactor: 싱글톤 제거에 따른, Class<?> 사용으로 변경

--- bank/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
+++ bank/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
@@ -17,26 +17,24 @@ public enum BusinessErrorCode implements ErrorResponseCode<BusinessException> {
      * 여기에 커스텀 BusinessException을 정의하면 됩니다.
      */
 
-    INVALID_PASSWORD_FORMAT_EXCEPTION(HttpStatus.BAD_REQUEST, InvalidPasswordFormatException.EXCEPTION),
-    INVALID_AUTH_INFO_EXCEPTION(HttpStatus.BAD_REQUEST, InvalidAuthInfoException.EXCEPTION);
+    INVALID_PASSWORD_FORMAT_EXCEPTION(HttpStatus.BAD_REQUEST, InvalidPasswordFormatException.class),
+    INVALID_AUTH_INFO_EXCEPTION(HttpStatus.BAD_REQUEST, InvalidAuthInfoException.class);
 
     private final HttpStatus status;
-    @Getter  private final BusinessException exception;
+    @Getter private final Class<? extends BusinessException> exception;
 
-    <T extends BusinessException> BusinessErrorCode(HttpStatus status, T exception){
+    BusinessErrorCode(HttpStatus status, Class<? extends BusinessException> eClass){
         this.status = status;
-        this.exception = exception;
+        this.exception = eClass;
     }
-
 
     @Override
     public HttpStatus getStatus() { return this.status; }
 
     @Override
-    public String getCode() { return exception.getErrorCode(); }
-
+    public String getCode() { throw new UnsupportedOperationException("You should use errorCode, message in the BusinessException"); }
     @Override
-    public String getMessage() { return exception.getMessage(); }
+    public String getMessage() { throw new UnsupportedOperationException("You should use errorCode, message in the BusinessException"); }
 
     public static ErrorResponseCode<BusinessException> find(BusinessException exception){
         BusinessErrorCode errorResponseCode =  map.get(exception.getClass());
@@ -46,12 +44,15 @@ public enum BusinessErrorCode implements ErrorResponseCode<BusinessException> {
         throw new IllegalArgumentException("Not mapped Exception");
     }
 
+    /**
+     * 커스텀 Exception에 맞는 ErrorCode를 매핑하는 저장소
+     */
     private static final Map<Class<? extends BusinessException>, BusinessErrorCode> map = new ConcurrentHashMap<>();
 
     static {
         Arrays.stream(BusinessErrorCode.values())
                 .forEach(errorCode -> {
-                    Class<? extends BusinessException> eClass = errorCode.exception.getClass();
+                    Class<? extends BusinessException> eClass = errorCode.exception;
                     map.put(eClass, errorCode);
                 });
     }
