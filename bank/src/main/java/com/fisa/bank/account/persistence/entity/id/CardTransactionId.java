bacf923f75bdd5e9dfabf3ce8b54457662fb5fb0
Ju-Min-Kyung <alsrud7767@naver.com>
2025-10-29 14:30:50 +0900

    feat: CardTransaction Id, JavaType, repository 정의

--- bank/src/main/java/com/fisa/bank/account/persistence/entity/id/CardTransactionId.java
+++ bank/src/main/java/com/fisa/bank/account/persistence/entity/id/CardTransactionId.java
@@ -1,4 +1,13 @@
 package com.fisa.bank.account.persistence.entity.id;
 
-public class CardTransactionId {
+import com.fisa.bank.common.persistence.id.BaseId;
+
+public class CardTransactionId extends BaseId<Long> {
+    private CardTransactionId(Long value){
+        super(value);
+    }
+
+    public static CardTransactionId of(Long value) {
+        return new CardTransactionId(value);
+    }
 }
--- bank/src/main/java/com/fisa/bank/account/persistence/entity/id/CardTransactionJavaType.java
+++ bank/src/main/java/com/fisa/bank/account/persistence/entity/id/CardTransactionJavaType.java
@@ -1,4 +1,16 @@
 package com.fisa.bank.account.persistence.entity.id;
 
-public class CardTransactionJavaType {
+import com.fisa.bank.common.persistence.id.BaseIdJavaType;
+import org.hibernate.type.descriptor.java.LongJavaType;
+import org.hibernate.type.descriptor.jdbc.BigIntJdbcType;
+
+public class CardTransactionJavaType extends BaseIdJavaType<Long, CardTransactionId> {
+    private CardTransactionJavaType() {
+        super(
+                CardTransactionId.class,
+                CardTransactionId::of,
+                LongJavaType.INSTANCE,
+                BigIntJdbcType.INSTANCE
+        );
+    }
 }
