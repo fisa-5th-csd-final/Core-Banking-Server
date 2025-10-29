a5d82a35397b110e02064a9bdf1863e00a055a2c
Bellmin <whdals011203@naver.com>
2025-10-29 17:37:43 +0900

    feat: CustomUserDetailsService 정의

---
+++ bank/src/main/java/com/fisa/bank/common/config/oauth2/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.fisa.bank.common.config.oauth2;
+
+import com.fisa.bank.user.persistence.entity.UserAuth;
+import com.fisa.bank.user.persistence.repository.UserAuthRepository;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserAuthRepository authRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserAuth userAuth = authRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username not found : %s", username)));
+
+        return new User(userAuth.getLoginId(), userAuth.getPassword(), Collections.emptyList());
+    }
+}
