65028dca63b5922d5d1fabb663f443e1952acb5b
Bellmin <whdals011203@naver.com>
2025-10-29 23:46:55 +0900

    feat: UserIdAuthenticationConverter 정의
    - loginId 와 password 를 받아서 Authentication 으로 변환

---
+++ bank/src/main/java/com/fisa/bank/common/config/oauth2/UserIdAuthenticationConverter.java
@@ -0,0 +1,45 @@
+package com.fisa.bank.common.config.oauth2;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+/**
+ * 사용자가 요청한 HTTP Request에서 자격 증명(ID/PW)을 꺼내서
+ * Authentication으로 만드는 역할
+ */
+@Component
+@RequiredArgsConstructor
+public class UserIdAuthenticationConverter implements AuthenticationConverter {
+
+    // RequestBody -> JSON 변환
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public Authentication convert(HttpServletRequest request) {
+
+        try(Reader reader = request.getReader()){
+            JsonNode jsonNode = objectMapper.readTree(reader);
+
+            String loginId = jsonNode.get("loginId").asText(null);
+            String password = jsonNode.get("password").asText(null);
+
+            Assert.notNull(loginId, "loginId should be not null");
+            Assert.notNull(password, "password should be not null");
+
+            return new UsernamePasswordAuthenticationToken(loginId, password);
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
