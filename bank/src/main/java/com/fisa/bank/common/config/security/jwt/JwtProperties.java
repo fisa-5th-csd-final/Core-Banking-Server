2eba79c64122d2921209f857a8f33a1697aa19f3
Bellmin <whdals011203@naver.com>
2025-10-31 16:25:11 +0900

    refactor: secretKey 삭제

--- bank/src/main/java/com/fisa/bank/common/config/security/jwt/JwtProperties.java
+++ bank/src/main/java/com/fisa/bank/common/config/security/jwt/JwtProperties.java
@@ -17,13 +17,11 @@ public class JwtProperties {
 
   private final AccessToken accessToken;
   private final RefreshToken refreshToken;
-  private final SecretKey secretKey; // TODO: at, rt 의 secret을 다르게 해야 할까?
 
   @ConstructorBinding
   public JwtProperties(AccessToken accessToken, RefreshToken refreshToken, String secretKey) {
     this.accessToken = accessToken;
     this.refreshToken = refreshToken;
-    this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
   }
 
   public record AccessToken(Duration expiry) {}
