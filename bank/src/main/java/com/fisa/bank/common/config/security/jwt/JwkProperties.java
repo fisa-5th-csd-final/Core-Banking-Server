a90b3442c5f1da94da5e1f6c14a582d4237d0b40
Bellmin <whdals011203@naver.com>
2025-10-31 14:42:55 +0900

    move: 패키지 이동 jwk -> jwt

--- bank/src/main/java/com/fisa/bank/common/config/security/jwt/JwkProperties.java
+++ bank/src/main/java/com/fisa/bank/common/config/security/jwt/JwkProperties.java
@@ -1,4 +1,4 @@
-package com.fisa.bank.common.config.security.jwk;
+package com.fisa.bank.common.config.security.jwt;
 
 import lombok.Getter;
 
@@ -6,6 +6,8 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.oauth2.jose.jws.JwsAlgorithm;
+import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
 
 @Getter
 @ConfigurationProperties(prefix = "jwk")
@@ -13,11 +15,13 @@ public class JwkProperties {
 
   private final PrivateKey privateKey;
   private final PublicKey publicKey;
+  private final String jwsAlgorithm;
 
   public JwkProperties(String publicKey, String privateKey) {
     //        this.privateKey = AsymmetricKeyUtils.createPrivateKey(privateKey, "RSA");
     //        this.publicKey = AsymmetricKeyUtils.createPublicKey(publicKey, "RSA");
     this.publicKey = null;
     this.privateKey = null;
+    this.jwsAlgorithm = JwsAlgorithms.RS256;
   }
 }
