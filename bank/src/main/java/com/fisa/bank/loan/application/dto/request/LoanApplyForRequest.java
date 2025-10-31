aa1a8895d789448252ad42b5ef60bd679cbbc360
cheongjay <wjdjoonim@gmail.com>
2025-10-31 18:59:01 +0900

    feat: 대출 가입 기능 추가

--- bank/src/main/java/com/fisa/bank/loan/application/dto/request/LoanApplyForRequest.java
+++ bank/src/main/java/com/fisa/bank/loan/application/dto/request/LoanApplyForRequest.java
@@ -2,10 +2,15 @@ package com.fisa.bank.loan.application.dto.request;
 
 import com.fisa.bank.loan.persistence.enums.InterestType;
 import com.fisa.bank.loan.persistence.enums.RepaymentType;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 public class LoanApplyForRequest {
@@ -13,14 +18,18 @@ public class LoanApplyForRequest {
     // 유저 아이디
 
     // 원금, 금리 유형
-    @NotNull
+    @NotNull(message = "원금을 입력해주세요.")
     private BigDecimal principal;
 
-    @NotNull
+    @NotNull(message = "금리 유형을 입력해주세요.")
     private InterestType interestType;
 
     // 상환 방법(원리금, 원금, 만기)
-    @NotNull
+    @NotNull(message = "상환 방법을 입력해주세요.")
     private RepaymentType repaymentType;
+
+    @NotNull(message = "상환 기간을 입력해주세요.")
+    @Min(value = 1, message = "상환 기간은 최소 1년 이상이어야 합니다.")
+    private Integer term;
 
 }
--- bank/src/main/java/com/fisa/bank/loan/persistence/entity/LoanLedger.java
+++ bank/src/main/java/com/fisa/bank/loan/persistence/entity/LoanLedger.java
@@ -1,10 +1,7 @@
 package com.fisa.bank.loan.persistence.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -29,6 +26,7 @@ import com.fisa.bank.user.persistence.entity.User;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
+@Getter
 public class LoanLedger {
 
   @Id
@@ -85,7 +83,6 @@ public class LoanLedger {
   @Column(nullable = false)
   private LocalDateTime nextRepaymentDate;
 
-  @Column(nullable = false)
   private LocalDateTime lastRepaymentDate;
 
   @Column(nullable = false)
