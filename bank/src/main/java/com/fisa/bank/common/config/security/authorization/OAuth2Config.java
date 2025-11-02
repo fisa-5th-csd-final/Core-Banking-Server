80056b669c82397362fa768a5c6f47998674a4c5
Bellmin <whdals011203@naver.com>
2025-11-02 23:28:57 +0900

    feat: OAuth2ClientAuthenticationProvider 추가

--- bank/src/main/java/com/fisa/bank/common/config/security/authorization/OAuth2Config.java
+++ bank/src/main/java/com/fisa/bank/common/config/security/authorization/OAuth2Config.java
@@ -1,15 +1,16 @@
 package com.fisa.bank.common.config.security.authorization;
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.authentication.ClientSecretAuthenticationProvider;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.token.DelegatingOAuth2TokenGenerator;
@@ -26,9 +27,14 @@ public class OAuth2Config {
   // 인메모리, JDBC 선택 가능
   @Bean
   public RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbc) {
-    return new JdbcRegisteredClientRepository(jdbc);
+      return new JdbcRegisteredClientRepository(jdbc);
   }
 
+  @Bean("OAuth2ClientAuthenticationProvider")
+  public AuthenticationProvider clientSecret(RegisteredClientRepository registeredClientRepository,
+                                             OAuth2AuthorizationService oAuth2AuthorizationService){
+      return new ClientSecretAuthenticationProvider(registeredClientRepository, oAuth2AuthorizationService);
+  }
   /** OAuth2Token 생성기 */
   @Bean
   public OAuth2TokenGenerator<?> tokenGenerator(JwtEncoder jwtEncoder) {
