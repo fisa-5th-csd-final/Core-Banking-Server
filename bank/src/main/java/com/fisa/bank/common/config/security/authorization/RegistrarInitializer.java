0c8a7b1edb44a6b4c74e44889d113184dbe8ebd0
Bellmin <whdals011203@naver.com>
2025-11-02 15:23:09 +0900

    feat: registrar client initializer 추가

---
+++ bank/src/main/java/com/fisa/bank/common/config/security/authorization/RegistrarInitializer.java
@@ -0,0 +1,38 @@
+package com.fisa.bank.common.config.security.authorization;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class RegistrarInitializer implements ApplicationRunner {
+
+    private final RegisteredClientRepository clientRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (clientRepository.findByClientId("registrar-client") == null) {
+            RegisteredClient client = RegisteredClient.withId(UUID.randomUUID().toString())
+                    .clientId("registrar-client")
+                    .clientSecret("{noop}secret")
+                    .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                    .scope("client.create")
+                    .scope("client.read")
+                    .build();
+            clientRepository.save(client);
+            log.info("Registrar Client 생성 성공");
+        } else {
+            log.info("Registrar Client가 이미 존재합니다. ");
+        }
+    }
+}
