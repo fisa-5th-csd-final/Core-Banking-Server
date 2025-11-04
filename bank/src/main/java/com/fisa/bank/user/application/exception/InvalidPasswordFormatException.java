f92069eb49d10cca586aa9172f4d8cb8eaa22d2d
Bellmin <whdals011203@naver.com>
2025-11-04 09:44:18 +0900

    refactor: errorCode 수정

--- bank/src/main/java/com/fisa/bank/user/application/exception/InvalidPasswordFormatException.java
+++ bank/src/main/java/com/fisa/bank/user/application/exception/InvalidPasswordFormatException.java
@@ -4,7 +4,7 @@ import com.fisa.bank.common.application.exception.BusinessException;
 
 public class InvalidPasswordFormatException extends BusinessException {
 
-  private static final String errorCode = "1001";
+  private static final String errorCode = "U1001";
   private static final String message = "비밀번호 규칙에 맞지 않습니다.";
 
   public InvalidPasswordFormatException() {
