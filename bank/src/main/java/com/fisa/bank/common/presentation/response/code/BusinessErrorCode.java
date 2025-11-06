8ab86470023c63b236542c90105e08a750efa5cb
cheongjay <wjdjoonim@gmail.com>
2025-11-06 16:05:41 +0900

    feat: 대출 상환 시 계좌 잔액 부족 예외 추가

--- bank/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
+++ bank/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
@@ -36,8 +36,9 @@ public enum BusinessErrorCode implements ErrorResponseCode<BusinessException> {
   PREFER_INTEREST_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, PreferInterestNotFoundException.class),
   LOAN_LEDGER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, LoanLedgerNotFoundException.class),
   LOAN_LEDGER_ACCESS_DENIED_EXCEPTION(HttpStatus.FORBIDDEN, LoanLedgerAccessDeniedException.class),
-  UNKNOWN_CALCULATOR_EXCEPTION(HttpStatus.BAD_REQUEST, UnknownCalculatorException.class);
-
+  UNKNOWN_CALCULATOR_EXCEPTION(HttpStatus.BAD_REQUEST, UnknownCalculatorException.class),
+  INSUFFICIENT_BALANCE_AMOUNT_EXCEPTION(
+      HttpStatus.UNPROCESSABLE_ENTITY, InSufficientBalanceAmountException.class);
   private final HttpStatus status;
   @Getter private final Class<? extends BusinessException> exception;
 
