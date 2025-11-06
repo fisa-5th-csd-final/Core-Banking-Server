0baa0214b6462785c414549d9e49deb5ddf79197
cheongjay <wjdjoonim@gmail.com>
2025-11-06 14:29:09 +0900

    feat: 지원하지 않는 계산기 예외 추가

--- bank/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
+++ bank/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
@@ -35,7 +35,8 @@ public enum BusinessErrorCode implements ErrorResponseCode<BusinessException> {
       HttpStatus.UNPROCESSABLE_ENTITY, InsufficientRepaymentException.class),
   PREFER_INTEREST_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, PreferInterestNotFoundException.class),
   LOAN_LEDGER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, LoanLedgerNotFoundException.class),
-  LOAN_LEDGER_ACCESS_DENIED_EXCEPTION(HttpStatus.FORBIDDEN, LoanLedgerAccessDeniedException.class);
+  LOAN_LEDGER_ACCESS_DENIED_EXCEPTION(HttpStatus.FORBIDDEN, LoanLedgerAccessDeniedException.class),
+  UNKNOWN_CALCULATOR_EXCEPTION(HttpStatus.BAD_REQUEST, UnknownCalculatorException.class);
 
   private final HttpStatus status;
   @Getter private final Class<? extends BusinessException> exception;
