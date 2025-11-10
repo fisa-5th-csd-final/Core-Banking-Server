692930cd61c99bcea559e03e87fc9c0ccbec81d7
cheongjay <wjdjoonim@gmail.com>
2025-11-10 18:11:29 +0900

    refactorr: 대출 상품 삭제 시 멱등성 반영

--- bank/src/main/java/com/fisa/bank/loan/persistence/repository/LoanRepository.java
+++ bank/src/main/java/com/fisa/bank/loan/persistence/repository/LoanRepository.java
@@ -1,15 +1,8 @@
 package com.fisa.bank.loan.persistence.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.fisa.bank.loan.persistence.entity.LoanProduct;
 import com.fisa.bank.loan.persistence.entity.id.LoanProductId;
 
-public interface LoanRepository extends JpaRepository<LoanProduct, LoanProductId> {
-  @Query(value = "SELECT * FROM loan_product WHERE loan_product_id = :id", nativeQuery = true)
-  Optional<LoanProduct> findByIdIgnoringRestriction(@Param("id") Long id);
-}
+public interface LoanRepository extends JpaRepository<LoanProduct, LoanProductId> {}
