9fd3202c55f24c711a1fa06229a94b5934f38457
Bellmin <whdals011203@naver.com>
2025-11-11 17:03:23 +0900

    spotlessApply

--- bank/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
+++ bank/src/main/java/com/fisa/bank/common/presentation/response/code/BusinessErrorCode.java
@@ -40,7 +40,8 @@ public enum BusinessErrorCode implements ErrorResponseCode<BusinessException> {
   DUPLICATE_LOAN_EXCEPTION(HttpStatus.CONFLICT, DuplicateLoanException.class),
   INSUFFICIENT_REPAYMENT_EXCEPTION(
       HttpStatus.UNPROCESSABLE_ENTITY, InsufficientRepaymentException.class),
-    INSUFFICIENT_BALANCE_AMOUNT_EXCEPTION(HttpStatus.NOT_ACCEPTABLE, InsufficientBalanceAmountException.class),
+  INSUFFICIENT_BALANCE_AMOUNT_EXCEPTION(
+      HttpStatus.NOT_ACCEPTABLE, InsufficientBalanceAmountException.class),
   PREFER_INTEREST_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, PreferInterestNotFoundException.class),
   LOAN_LEDGER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, LoanLedgerNotFoundException.class),
   LOAN_LEDGER_ACCESS_DENIED_EXCEPTION(HttpStatus.FORBIDDEN, LoanLedgerAccessDeniedException.class),
