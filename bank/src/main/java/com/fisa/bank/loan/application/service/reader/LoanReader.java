0cf0fa063076a2c75b1d0d778d9bc4f2f76dd367
cheongjay <wjdjoonim@gmail.com>
2025-11-19 12:18:01 +0900

    fix: 상환 완료된 대출은 조회되지 않도록 수정

--- bank/src/main/java/com/fisa/bank/loan/application/service/reader/LoanReader.java
+++ bank/src/main/java/com/fisa/bank/loan/application/service/reader/LoanReader.java
@@ -15,6 +15,7 @@ import com.fisa.bank.loan.persistence.entity.LoanLedger;
 import com.fisa.bank.loan.persistence.entity.LoanProduct;
 import com.fisa.bank.loan.persistence.entity.id.LoanLedgerId;
 import com.fisa.bank.loan.persistence.entity.id.LoanProductId;
+import com.fisa.bank.loan.persistence.enums.RepaymentStatus;
 import com.fisa.bank.loan.persistence.repository.LoanLedgerRepository;
 import com.fisa.bank.loan.persistence.repository.LoanRepository;
 import com.fisa.bank.user.persistence.entity.id.UserId;
@@ -37,8 +38,10 @@ public class LoanReader {
   }
 
   public LoanLedger findLoanLedgerById(Long loanLedgerId) {
+    List<RepaymentStatus> excluded = List.of(RepaymentStatus.COMPLETED, RepaymentStatus.TERMINATED);
+
     return loanLedgerRepository
-        .findById(LoanLedgerId.of(loanLedgerId))
+        .findByLoanLedgerIdAndRepaymentStatusNotIn(LoanLedgerId.of(loanLedgerId), excluded)
         .orElseThrow(() -> new LoanLedgerNotFoundException(loanLedgerId));
   }
 
