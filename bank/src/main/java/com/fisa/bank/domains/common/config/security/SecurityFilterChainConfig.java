99bbbc798edc40e8e4fd2a3d8ba02dfa1aed1486
Bellmin <whdals011203@naver.com>
2025-11-19 12:00:40 +0900

    refactor: swagger endpoint 수정

--- bank/src/main/java/com/fisa/bank/domains/common/config/security/SecurityFilterChainConfig.java
+++ bank/src/main/java/com/fisa/bank/domains/common/config/security/SecurityFilterChainConfig.java
@@ -106,8 +106,8 @@ public class SecurityFilterChainConfig {
                         "/api/loans/products",
                         "/api/loans/{loanProductId:\\d+}",
                         "/api/interests/{loanProductId:\\d+}",
-                        "/swagger-ui/index.html", // TODO: Swagger 전용 필터체인으로 분리
-                        "/v3/api-docs", // TODO: Swagger 전용 필터체인으로 분리
+                        "/swagger-ui/**",// TODO: Swagger 전용 필터체인으로 분리
+                        "/v3/api-docs/**", // TODO: Swagger 전용 필터체인으로 분리
                         "/swagger-resources/**" // TODO: Swagger 전용 필터체인으로 분리
                         )
                     .requestMatchers(HttpMethod.POST, "/api/loans", "/api/login", "/api/users")
