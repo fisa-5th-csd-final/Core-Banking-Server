2d074b2799e5ea3262cc267e553d5eeba750a010
JJ <113548427+cheongjay@users.noreply.github.com>
2025-11-28 17:55:02 +0900

    [REFACTOR] 월 상환 시 마지막 상환일 경우 nextRepaymentDate는 null 처리 (#128)
    * refactor: 월 상환 시 마지막 상환일 경우 nextRepaymentDate는 null 처리
    
    * gemini review 반영
    
    * style:: 필요없는 출력문 제거

--- bank/src/main/java/com/fisa/bank/domains/loan/application/service/LoanService.java
+++ bank/src/main/java/com/fisa/bank/domains/loan/application/service/LoanService.java
@@ -274,13 +274,21 @@ public class LoanService {
     // 남은 원금, 다음 상환일, 마지막 상환 날짜 업데이트
     LocalDateTime lastRepaymentDate = loanLedger.getNextRepaymentDate();
     LocalDateTime nextRepaymentDate = lastRepaymentDate.plusMonths(1);
+    RepaymentStatus repaymentStatus = loanLedger.getRepaymentStatus();
+
+    // 만약 이번 상환일이 마지막 상환일이라면 다음 상환일 null로 처리
+    // 대출 상태 COMPLETED도 변경
+    if (totalPrincipal.compareTo(loanLedger.getRemainPrincipal()) == 0) {
+      nextRepaymentDate = null;
+      repaymentStatus = RepaymentStatus.COMPLETED;
+    }
 
     loanLedger.updateLoanLedger(
         UpdateLoanLedgerParam.builder()
             .remainPrincipal(monthlyRepayment.getRemainPrincipal())
             .lastRepaymentDate(monthlyRepayment.getRepaymentDate())
             .nextRepaymentDate(nextRepaymentDate)
-            .status(loanLedger.getRepaymentStatus())
+            .status(repaymentStatus)
             .build());
 
     // 이력성 테이블에도 저장
