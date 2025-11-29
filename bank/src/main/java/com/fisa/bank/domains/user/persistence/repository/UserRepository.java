b813d29b37dd75e883d2b77bc5f89e42431abf1d
Bellmin <whdals011203@naver.com>
2025-11-29 12:28:15 +0900

    refactor: EntityGraph 사용

--- bank/src/main/java/com/fisa/bank/domains/user/persistence/repository/UserRepository.java
+++ bank/src/main/java/com/fisa/bank/domains/user/persistence/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package com.fisa.bank.domains.user.persistence.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fisa.bank.domains.user.persistence.entity.User;
 import com.fisa.bank.domains.user.persistence.entity.id.UserId;
 
-public interface UserRepository extends JpaRepository<User, UserId> {}
+public interface UserRepository extends JpaRepository<User, UserId> {
+
+  @EntityGraph(attributePaths = {"userAuth"})
+  List<User> findAll();
+}
