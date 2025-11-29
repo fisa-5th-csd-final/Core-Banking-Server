cd9901bd303d9e1246fac6021fae0d7434d732ba
Bellmin <whdals011203@naver.com>
2025-11-29 11:33:46 +0900

    fix: JPQL 로 변경

--- bank/src/main/java/com/fisa/bank/domains/user/persistence/repository/UserAuthRepository.java
+++ bank/src/main/java/com/fisa/bank/domains/user/persistence/repository/UserAuthRepository.java
@@ -14,5 +14,6 @@ public interface UserAuthRepository extends JpaRepository<UserAuth, String> {
   @Query("SELECT ua.user.userId FROM UserAuth ua WHERE ua.loginId=:loginId")
   Optional<UserId> findUserIdByLoginId(@Param("loginId") String loginId);
 
-  Optional<UserAuth> findByUserId(UserId userId);
+  @Query("select ua from UserAuth ua where ua.user.userId = :userId")
+  Optional<UserAuth> findByUserId(@Param("userId") UserId userId);
 }
